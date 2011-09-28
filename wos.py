26aa3c27ea303a5be29e5c36a0d759192a46bc02
Brian Pitts <brian@polibyte.com>
2011-09-28 11:53:48 -0400

    Use configured port number in javascript. Style fixes as well.

--- wos.py
+++ wos.py
@@ -9,39 +9,6 @@ import tornado.web
 import subprocess
 import sys
 
-TEMPLATE = """
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Wall</title>
-    <script type="text/javascript" src="http://code.jquery.com/jquery-1.6.4.min.js"></script>
-</head>
-<body>
-    <h1>Wall</h1>
-    <div id="wall"></div>
-    <script type="text/javascript">
-        $(document).ready(function(){
-            if ("WebSocket" in window) {
-              var ws = new WebSocket("ws://localhost:8080/websocket/");
-              ws.onmessage = function (evt) {
-                  var html = evt.data + "<br>"
-                  $("#wall").append(html);
-              };
-              ws.onclose = function() {
-                  $("#wall").append("<p>That's all, folks!</p>");
-              };
-            } else {
-                  $("#wall").html("<p>Sorry, your browser is not supported.</p>");
-            }
-        });
-    </script>
-</body>
-</html>
-"""
-
-
-LISTENERS = []
-
 def read_config():
     if os.path.isfile('wos.ini'):
         parser = SafeConfigParser()
@@ -57,23 +24,23 @@ def monitor():
     while p.poll() is None:
         line = p.stdout.readline()
         if line:
-            for c in LISTENERS:
+            for c in listeners:
                 c.write_message(unicode(line))
 
 
 class IndexHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write(TEMPLATE)
+        self.write(template)
 
 class WebSocketHandler(tornado.websocket.WebSocketHandler):
     def open(self):
-        LISTENERS.append(self)
+        listeners.append(self)
 
     def on_message(self, message):
         pass
 
     def on_close(self):
-        LISTENERS.remove(self)
+        listeners.remove(self)
 
 application = tornado.web.Application([
     (r'/', IndexHandler),
@@ -82,9 +49,42 @@ application = tornado.web.Application([
 
 
 if __name__ == "__main__":
+    listeners = []
     config = read_config()
     port = config.get('tornado', 'port')
+
+    template = """
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Wall</title>
+        <script type="text/javascript" src="http://code.jquery.com/jquery-1.6.4.min.js"></script>
+    </head>
+    <body>
+        <h1>Wall</h1>
+        <div id="wall"></div>
+        <script type="text/javascript">
+            $(document).ready(function(){
+                if ("WebSocket" in window) {
+                  var ws = new WebSocket("ws://localhost:%s/websocket/");
+                  ws.onmessage = function (evt) {
+                      var html = evt.data + "<br>"
+                      $("#wall").append(html);
+                  };
+                  ws.onclose = function() {
+                      $("#wall").append("<p>That's all, folks!</p>");
+                  };
+                } else {
+                      $("#wall").html("<p>Sorry, your browser is not supported.</p>");
+                }
+            });
+        </script>
+    </body>
+    </html>
+    """ % port
+
     threading.Thread(target=monitor).start()
+
     http_server = tornado.httpserver.HTTPServer(application)
     http_server.listen(port)
     tornado.ioloop.IOLoop.instance().start()
